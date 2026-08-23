80c31a3466ca5ac7bddd70c9430c041faafa3ecc
harshalmandliya <harshalmandliya3@gmail.com>
2026-08-23 19:16:57 +0530

    feat: enable user validation while adding a product to cart

--- order/src/main/java/com/ecommerce/order/service/CartService.java
+++ order/src/main/java/com/ecommerce/order/service/CartService.java
@@ -2,8 +2,10 @@ package com.ecommerce.order.service;
 
 
 import com.ecommerce.order.clients.ProductServiceClient;
+import com.ecommerce.order.clients.UserServiceClient;
 import com.ecommerce.order.dto.CartItemRequest;
 import com.ecommerce.order.dto.ProductResponse;
+import com.ecommerce.order.dto.UserResponse;
 import com.ecommerce.order.model.CartItem;
 import com.ecommerce.order.repository.CartItemRepository;
 import jakarta.transaction.Transactional;
@@ -20,6 +22,7 @@ import java.util.Optional;
 public class CartService {
     private final CartItemRepository cartItemRepository;
     private final ProductServiceClient productServiceClient;
+    private final UserServiceClient userServiceClient;
 
     public boolean addToCart(Long userId, CartItemRequest request) {
 
@@ -30,11 +33,9 @@ public class CartService {
         if (productResponse.getStockQuantity() < request.getQuantity())
             return false;
 
-//        Optional<User> userOpt = userRepository.findById(Long.valueOf(userId));
-//        if (userOpt.isEmpty())
-//            return false;
-//
-//        User user = userOpt.get();
+        UserResponse userResponse = userServiceClient.getUserDetails(String.valueOf(userId));
+        if(userResponse == null) return false;
+
 
         CartItem existingCartItem = cartItemRepository.findByUserIdAndProductId(userId, request.getProductId());
         if (existingCartItem != null) {
