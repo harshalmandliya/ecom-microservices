782f4a2984311ef61129e8babac0d913118bcc1b
harshalmandliya <harshalmandliya3@gmail.com>
2026-09-17 01:45:57 +0530

    feat: implement rate limiting in API gateway using Spring Cloud Gateway

--- gateway/src/main/java/com/ecommerce/gateway/GateWayConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/GateWayConfig.java
@@ -1,30 +1,58 @@
 package com.ecommerce.gateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GateWayConfig {
 
     @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(10,20,1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getHostName());
+    }
+
+    @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("product-service",r->r
-                                .path("/api/products/**")
-                                .filters(f->f.circuitBreaker(config->config
+                .route("product-service", r -> r
+                        .path("/api/products/**")
+                        .filters(f -> f
+                                .circuitBreaker(config -> config
                                         .setName("ecomBreaker")
                                         .setFallbackUri("forward:/fallback/products")
-                                ))
-                                .uri("http://localhost:8084"))
+                                )
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(hostNameKeyResolver())
+                                )
+                        )
+                        .uri("http://localhost:8084")
+                )
 
                 .route("user-service",r->r
                         .path("/api/users/**")
                         .filters(f->f.circuitBreaker(config->config
                                 .setName("ecomBreaker")
                                 .setFallbackUri("forward:/fallback/users")
-                        ))
+                        )
+
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(hostNameKeyResolver())
+                                )
+
+                        )
                         .uri("http://localhost:8083"))
 
                 .route("order-service",r->r
@@ -32,7 +60,12 @@ public class GateWayConfig {
                         .filters(f->f.circuitBreaker(config->config
                                 .setName("ecomBreaker")
                                 .setFallbackUri("forward:/fallback/orders")
-                        ))
+                        )
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(hostNameKeyResolver())
+                                )
+                        )
                         .uri("http://localhost:8085"))
 
                 .route("eureka",r->r
